377d108ce5dd9be807d73987395980769f08751d
Ваня <7328652@gmail.com>
2023-08-03 06:44:40 +0300

    поправив код

--- src/js/hero-parallax.js
+++ src/js/hero-parallax.js
@@ -3,3 +3,13 @@ VanillaTilt.init(document.querySelectorAll('.hero-image-container'), {
   speed: 400,
   glare: false,
 });
+
+let btnEl = document.querySelector('.hero-btn');
+btnEl.addEventListener('click', scrollToAnchor);
+
+function scrollToAnchor() {
+  window.scroll({
+    top: document.querySelector('#about').offsetTop,
+    behavior: 'smooth',
+  });
+}
