98525be020644e2904ab2dfdc9e38fb2c2b18273
Taras Popovych <128419444+TarasP0p0vych@users.noreply.github.com>
2023-08-04 13:23:38 +0300

    Revert "Upd"
    This reverts commit 95287a4511f54607281716699dfac8194e026535, reversing
    changes made to 740ce0efef314d4cbc9f2d6c7a796bbbc6647bbd.

--- src/js/scroll-trigger.js
+++ src/js/scroll-trigger.js
@@ -1,11 +1,31 @@
+<<<<<<< Updated upstream
+// Trigger CSS animations on scroll.
+// Detailed explanation can be found at http://www.bram.us/2013/11/20/scroll-animations/
+
+// Looking for a version that also reverses the animation when
+// elements scroll below the fold again?
+// --> Check https://codepen.io/bramus/pen/vKpjNP
+
+=======
+>>>>>>> Stashed changes
 jQuery(function ($) {
   var doAnimations = function () {
     // Calc current offset and get all animatables
     var offset = $(window).scrollTop() + $(window).height(),
       $animatables = $('.animatable');
+<<<<<<< Updated upstream
+
+    // Unbind scroll handler if we have no animatables
     if ($animatables.length == 0) {
       $(window).off('scroll', doAnimations);
     }
+
+    // Check all animatables and animate them if necessary
+=======
+    if ($animatables.length == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+>>>>>>> Stashed changes
     $animatables.each(function (i) {
       var $animatable = $(this);
       if ($animatable.offset().top + $animatable.height() - 20 < offset) {
